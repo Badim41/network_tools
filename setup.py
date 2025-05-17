6ded7353e425f343afc5c44f83b22e8524d27da0
badim41 <ramdomakk@gmail.com>
2025-05-18 08:16:47 +1200

    исправлен 'tts_api'

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='network_tools',
-    version='0.6',
+    version='0.61',
     packages=find_packages(),
     install_requires=[
         'requests',
