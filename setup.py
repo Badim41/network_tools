660ad03943a4309a05b240e7e06832d2f1bcea45
badim41 <ramdomakk@gmail.com>
2025-06-30 15:23:29 +1200

    little fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='network_tools',
-    version='0.93',
+    version='0.94',
     packages=find_packages(),
     install_requires=[
         'requests',
